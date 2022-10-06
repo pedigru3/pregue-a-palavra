12d366ebedfd332be200021bda42335b2bb2f8fb
Felipe Ferreira <contosdeferreira@hotmail.com>
2022-10-06 07:53:34 -0300

    teste

--- lib/src/pages/bible/result/bible_result.dart
+++
@@ -1,8 +0,0 @@
-import 'package:freezed_annotation/freezed_annotation.dart';
-import 'package:pregue_a_palavra/src/models/bible_model.dart';
-part 'bible_result.freezed.dart';
-
-class BibleResult with _$BibleResult {
-  factory BibleResult.success(BibleModel bible) = Success;
-  factory BibleResult.error(String msg) = Error;
-}
